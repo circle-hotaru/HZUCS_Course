c272f21f86795d9192e71accd8cb16aec9b78f6f
circlehotarux <mightyherox@gmail.com>
2020-05-12 15:42:17 +0800

    调整目录结构

--- 网络通信程序设计_HZU_2019spring/实验_HZU_2019spring/experiment6/ServerTest/src/me/circlehotarux/multicast/ServerTest.java
+++
@@ -1,7 +0,0 @@
-package me.circlehotarux.multicast;
-
-public class ServerTest {
-    public static void main(String[] args) throws java.io.IOException {
-        new SonServerThreadTest().start();
-    }
-}
